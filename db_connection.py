b8cd9592fb1ec8d8c177acda97d3dac9b83b7849
oINCLUDEo <nikita-zhuklin@rambler.ru>
2024-10-18 01:03:44 +0400

    added categories support and logging

--- db_connection.py
+++ db_connection.py
@@ -14,6 +14,7 @@ DB_PORT = os.getenv('DB_PORT')
 
 
 def get_users():
+    query = "SELECT user_tg_id FROM users;"
     # Подключение к базе данных postgres
     with psycopg2.connect(
             dbname=DB_NAME,
@@ -23,13 +24,74 @@ def get_users():
             port=DB_PORT
     ) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT user_id FROM users;")
+            cur.execute(query)
             users = [user[0] for user in cur.fetchall()]
 
             return users
 
 
+def get_channels():
+    query = "SELECT channel_tg_id FROM channels;"
+    # Подключение к базе данных postgres
+    with psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            channels = [channel[0] for channel in cur.fetchall()]
+
+            return channels
+
+
+def get_category_users(category_id):
+    query = """
+            SELECT user_tg_id FROM users u 
+            JOIN user_categories uc ON u.user_id = uc.user_id 
+            WHERE uc.category_id IN %s
+            GROUP BY user_tg_id;
+            """
+    # Подключение к базе данных postgres
+    with psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query, (category_id,))
+            users = [user[0] for user in cur.fetchall()]
+
+            return users
+
+
+def get_channel_category(channel_tg_id):
+    query = """
+            SELECT category_id FROM channel_categories cc
+            JOIN channels c ON c.channel_id = cc.channel_id
+            WHERE c.channel_tg_id = %s;
+            """
+    # Подключение к базе данных postgres
+    with psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+    ) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query, (channel_tg_id,))
+            categories = [category[0] for category in cur.fetchall()]
+
+            return categories
+
+
 def add_user(user_id):
+    query = "INSERT INTO users(user_tg_id) VALUES (%s);"
     try:
         # Подключение к базе данных postgres
         with psycopg2.connect(
@@ -40,7 +102,7 @@ def add_user(user_id):
                 port=DB_PORT
         ) as conn:
             with conn.cursor() as cur:
-                cur.execute("INSERT INTO users(user_id) VALUES (%s);", (user_id,))
+                cur.execute(query, (user_id,))
 
         return "Пользователь успешно добавлен!"
     except psycopg2.IntegrityError as e:
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import asyncio
+import logging
 from helpers import remove_file
-from db_connection import get_users, add_user
+from db_connection import *
 
-from aiogram.utils.media_group import MediaGroupBuilder
-from telethon import TelegramClient, events, utils
 from aiogram import Bot, Dispatcher, types
+from aiogram.utils.media_group import MediaGroupBuilder
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from telethon import TelegramClient, events, utils
 from dotenv import load_dotenv
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, filename="tgnews.log", filemode="w")
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -16,21 +20,23 @@ api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
 
-# Создание бота и диспетчера
+# Создание бота, диспетчера и клиента
 bot = Bot(token=bot_token)
 dp = Dispatcher()
-
 client = TelegramClient('news_parser', api_id, api_hash)
-client.parse_mode = 'html'
 
 users = get_users()
+channels = get_channels()
 
 
 # TODO: Имеется проблема с видео: нет превью и размер превью видео маленький квадратик
-@client.on(events.Album(chats=[1074845292, 1457994499, 2228559702]))
+@client.on(events.Album(chats=channels))
 async def albums_handler(event):
     media_group = MediaGroupBuilder(caption=f'{event.text}')
     filename_list = []
+
+    msg_from_channel_id = (await event.get_sender()).id
+    target_users = get_category_users(tuple(get_channel_category(msg_from_channel_id)))
 
     for file in event.messages:
         filename = await file.download_media()
@@ -44,41 +50,46 @@ async def albums_handler(event):
             media_group.add(type='photo',
                             media=types.FSInputFile(path=filename),
                             parse_mode=ParseMode.HTML)
-    for user in users:
+    for user in target_users:
         await bot.send_media_group(chat_id=user, media=media_group.build())
     remove_file(filename_list)
 
 
-@client.on(events.NewMessage(chats=[1074845292, 1457994499, 2228559702]))
+@client.on(events.NewMessage(chats=channels))
 async def my_event_handler(event):
     # Проверка наличия медиафайлов в сообщении
     if event.media:
         if not event.grouped_id:
+            msg_from_channel_id = (await event.get_sender()).id
+            target_users = get_category_users(tuple(get_channel_category(msg_from_channel_id)))
+
             filename = await event.download_media()
             # Отправка группы фото и текста в одном сообщении
             # TODO @INCLUDE: В telethon нет поддержки
             if utils.is_video(filename):
-                for user in users:
+                for user in target_users:
                     await bot.send_video(chat_id=user,
                                          video=types.FSInputFile(path=filename),
                                          caption=f'{event.text}',
                                          parse_mode=ParseMode.HTML)
             elif utils.is_image(filename):
-                for user in users:
+                for user in target_users:
                     await bot.send_photo(chat_id=user,
                                          photo=types.FSInputFile(path=filename),
                                          caption=f'{event.text}',
                                          parse_mode=ParseMode.HTML)
             elif utils.is_gif(filename):
-                for user in users:
+                for user in target_users:
                     await bot.send_animation(chat_id=user,
                                              animation=types.FSInputFile(path=filename),
                                              caption=f'{event.text}',
                                              parse_mode=ParseMode.HTML)
             remove_file(filename)
     else:
+        msg_from_channel_id = event.peer_id.channel_id
+        target_users = get_category_users(tuple(get_channel_category(msg_from_channel_id)))
         # Отправка только текста, если медиафайлов нет
-        for user in users:
+        for user in target_users:
             await bot.send_message(chat_id=user,
                                    text=f'{event.text}',
                                    parse_mode=ParseMode.HTML)
@@ -90,13 +101,16 @@ async def cmd_start(message: types.Message):
 
 
 async def start_telethon():
-    print("Channels parser started")
+    client.parse_mode = 'html'
     await client.start()
+    print("Channels parser started")
+    logging.info("Парсер новостных каналов успешно запущен")
     await client.run_until_disconnected()
 
 
 async def start_aiogram():
     print("Bot started")
+    logging.info("Бот успешно запущен")
     await dp.start_polling(bot)
 
 
@@ -107,8 +121,14 @@ async def main():
 # Фрагменты кода функций запуска взяты из GPT
 
 if __name__ == '__main__':
-    # Получение текущего цикла событий
-    loop = asyncio.get_event_loop()
+    try:
+        # Создание цикла событий
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
 
-    # Запуск основной функции в текущем цикле событий
-    loop.run_until_complete(main())
+        # Запуск основной функции в текущем цикле событий
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        logging.info("Выполнение скрипта прервано пользователем.")
+    finally:
+        logging.info("Завершение работы скрипта.")
